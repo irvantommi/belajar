5bf68f33e01677b2fab1f16a26bde0eac79afb2a
Irvan Tommi <irvan@alodokter.com>
2025-06-13 14:44:31 +0700

    Refine CORS middleware to allow specific origin and credentials for localhost; enhance security for development environment.

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -13,7 +13,11 @@ import (
 // CORS middleware
 func withCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin == "http://localhost:3000" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
